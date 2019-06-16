6c570393056d3202342cafa4a4ab0add1613ddab
Donald Boulton <donaldboulton@gmail.com>
2019-06-16 11:29:31 -0500

    adding web push

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
-const port = process.env.PORT || 3131
-
+const bodyParser = require('body-parser')
+const webpush = require('web-push')
+const app = express()
 app.use(cors())
-
-app.get('/api', (req, res) => {
-  res.status(200).json({api: 'version 1'})
+app.use(bodyParser.json())
+const port = 4000
+app.get('/', (req, res) => res.send('Hello World!'))
+const dummyDb = { subscription: null } //dummy in memory store
+const saveToDatabase = async subscription => {
+  // Since this is a demo app, I am going to save this in a dummy in memory store. Do not do this in your apps.
+  // Here you should be writing your db logic to save it.
+  dummyDb.subscription = subscription
+}
+// The new /save-subscription endpoint
+app.post('/save-subscription', async (req, res) => {
+  const subscription = req.body
+  await saveToDatabase(subscription) //Method to save the subscription to Database
+  res.json({ message: 'success' })
 })
-
-app.use((req, res) => res.status(404).send({code: '404', message: 'no found'}))
-
-app.listen(port, () => console.log('server started on port', port))
+const vapidKeys = {
+  publicKey:
+    'BGGMV7bJQRiPlAsn5MH6DPMik5MUOR8o7LS6-TjNe-pMdX_HtvniEgE--LEQAXn3V3X1N-jaAfWIjWNTCGiekGE',
+  privateKey: 
+    'SuuHV2S__C2907ho39MCjTF1BSkdSttnVcENeB14wnE',
+}
+//setting our previously generated VAPID keys
+webpush.setVapidDetails(
+  'mailto:donaldboulton@gmail.com',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+)
+//function to send the notification to the subscribed device
+const sendNotification = (subscription, dataToSend) => {
+  webpush.sendNotification(subscription, dataToSend)
+}
+//route to test send notification
+app.get('/send-notification', (req, res) => {
+  const subscription = dummyDb.subscription //get subscription from your databse here.
+  const message = 'Hello World'
+  sendNotification(subscription, message)
+  res.json({ message: 'message sent' })
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
